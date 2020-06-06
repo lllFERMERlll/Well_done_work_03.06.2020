87eba3cc36f668c52f1108cbedfd415ede7e46c3
FERMER T^T <FERMER T^T@DESKTOP-DH123RV>
2020-06-06 17:31:59 +0300

    Добавьте файлы проекта.

---
+++ Well_done_work_03.06.2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Well_done_work_03._06._2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
